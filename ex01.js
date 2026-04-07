50a551fa6f3c2ed80f39773c180223983384b64d
MiguelHolanda <miguelalmeidaholanda@gmail.com>
2026-04-07 15:15:18 -0300

    feat: adicinei o ex1

---
+++ ex01.js
@@ -0,0 +1,17 @@
+// const readline = require('readline-sync')
+
+// let nome = readline.question('digite seu nome: ')
+
+// console.log(`seu nome é : ${nome}`)
+
+const readline = require('readline-sync')
+
+let nome = readline.question('informe se nome: ')
+let curso = readline.question('informe o curso que esta matriculado: ')
+let idade = readline.question('informe sua idade: ')
+console.log('')
+console.log('====== SISTEMA DE CADASTRO ======')
+console.log('')
+console.log(`nome do usuario: ${nome}`)
+console.log(`curso do usuario: ${curso}`)
+console.log(`idade  do usuario: ${idade}`)
